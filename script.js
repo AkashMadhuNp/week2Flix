78a5a1dd8dbb050eaf3f64ccb84e88319acf62ba
Blyaax <150952601+Blyaax@users.noreply.github.com>
2024-04-11 18:14:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function validateEmail() {
+    var email = document.getElementById('email').value;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
